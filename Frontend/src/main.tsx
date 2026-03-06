3f62300943ea6956584bc77b7a263ee5519419c6
Shreya Awari <shreyaawari31@gmail.com>
2026-03-06 21:43:50 +0530

    GeoWatch Admin Dashboard Map & Monitoring Features

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import 'leaflet/dist/leaflet.css'
 import App from './App.tsx'
 
+(window as any).global = window;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
